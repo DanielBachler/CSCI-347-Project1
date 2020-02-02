5149c86cdf70f366ba882bd6f79a09bf4a315ff6
Dan Bachler <daniel.bachler@comcast.net>
2020-02-02 13:00:45 -0700

    First method done

--- method.py
+++ method.py
@@ -1,9 +1,23 @@
 # Dan Bachler
+# Janet Madrid
+import numpy as np
+import pandas as pd
 
+data = pd.read_csv("poker-hand-training-true.data", sep = ",", header = None)
+data = data.to_numpy()
+#print(data)
 # Multi-dim mean function
 def multi_dim_mean(arr):
-    pass
-
+    # Get average of all columns
+    size = arr.shape
+    total = 0
+    for i in range(0, size[1]):
+        column_total = 0
+        for j in range(0, size[0]):
+            column_total += data[j][i]
+        total += column_total / size[0]
+    return total / size[1]
+    
 # Covar function
 def covariance(arr1, arr2):
     pass
@@ -23,3 +37,5 @@ def stanard_norm(arr):
 # Covar matrix function
 def covar_matrix(arr):
     pass
+
+print(multi_dim_mean(data))
